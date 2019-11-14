df236fd304feeeceb705d6b17b31a73dba772010
Hanjun Kim <hallazzang@gmail.com>
2019-11-14 11:53:48 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import find_packages, setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="ikakao",
+    version="0.0.1",
+    description="Kakao i",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Hanjun Kim",
+    author_email="hallazzang@gmail.com",
+    python_requires=">=3.6",
+    url="https://github.com/hallazzang/ikakao",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+    ],
+)
+
